31a640437e955b2c5bf17030ae6f626f6fa60431
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-01 15:35:33 -0300

    create home component to be the initial one

---
+++ src/components/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+	<v-parallax
+		src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+	>
+	<v-card class="mx-25">
+		<v-card-title>Alphabeta - serviços em segurança</v-card-title>
+	</v-card>
+	</v-parallax>
+
+</template>
