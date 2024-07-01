580ec23a00b9851fc2ebd91d643c67cfb2c920c9
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-01 14:48:05 -0300

    fix header alignment

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,17 @@
 <template>
-	<v-app-bar class="bg-alphabeta-red">
-		<v-toolbar-title>My App</v-toolbar-title>
-		<v-spacer />
-		<v-btn class="bg-something">Home</v-btn>
-		<v-btn >About</v-btn>
-		<v-btn >Contact</v-btn>
+	<v-app-bar class="text-center bg-alphabeta-red">
+		<v-row class="mx-50">
+			<v-col class="text-end">
+				<v-btn>Alphabeta</v-btn>
+			</v-col>
+			<v-col class="text-start ml-0">
+				<v-btn>Home</v-btn>
+				<v-btn>About</v-btn>
+				<v-btn>Contact</v-btn>
+				<v-btn>Home</v-btn>
+				<v-btn>About</v-btn>
+				<v-btn>Contact</v-btn>
+			</v-col>
+		</v-row>
 	</v-app-bar>
 </template>
