be2b8c3305f2789c799cddbf1cfabdfab23b83f6
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-01 18:09:58 -0300

    fix card position in home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,13 @@
 <template>
-	<v-parallax
-		src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-	>
-	<v-card class="mx-25">
-		<v-card-title>Alphabeta - serviços em segurança</v-card-title>
-	</v-card>
-	</v-parallax>
-
+	<div>
+		<div>	<v-img
+			src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_960_720.jpg"
+		></v-img></div>
+	
+		<div class="mt-n8 position-relative mx-16 d-sm-mx-8">
+			<v-sheet class="mx-16">
+				<div class="text-h5">ALPHABETA - SOLUÇÕES EM SEGURANÇA</div>
+			</v-sheet>
+		</div>
+	</div>
 </template>
