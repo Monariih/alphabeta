b0af3b2aa9e44fc4c807f191bc7437bf76bee298
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-02 17:00:45 -0300

    set homepage card as a grid component

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -7,40 +7,40 @@
 			></v-parallax>
 		</div>
 	
-		<div class="mx-auto w-xs-100 w-sm-75 w-xl-50  justify-center">
-			<v-card class="mx-lg-16 px-lg-16">
-				<div class="text-center">
-					<v-card-title>
-						ALPHABETA
-						<v-card-subtitle>
-							SOLUÇÕES EM SEGURANÇA ESPECIALIZADA
-						</v-card-subtitle>
-					</v-card-title>
-				</div>
-				<v-divider class="mx-16"></v-divider>
-				<v-card-text>
-					<div class="text-justify text-body-1">
-						A ALPHABETA oferece mão de obra especializada e treinada para atender todas as demandas
-					em serviços terceirizados, tanto para condomínios quanto para empresas. Com mais de 10
-					anos de experiência, entendemos profundamente a importância da transformação digital e
-					das novas dinâmicas nas relações comerciais e sociais no mundo dos negócios.
-					Estamos comprometidos em oferecer soluções inovadoras e eficientes para nossos clientes,
-					adaptando-nos às necessidades do mercado atual. Nossos profissionais são cuidadosamente
-					selecionados e continuamente treinados para garantir a mais alta qualidade de serviço,
-					proporcionando segurança, confiabilidade e excelência operacional.
-					Além disso, utilizamos tecnologias de ponta para otimizar processos e garantir uma gestão
-					eficaz dos recursos, sempre visando a satisfação do cliente e a melhoria contínua. Com a ALPHABETA,
-					você pode contar com um parceiro que entende suas necessidades e está preparado para enfrentá-las
-					com competência e dedicação.
-					</div>
-				</v-card-text>
-			</v-card>
-		</div>
-
-		<div class="">
-			<v-parallax
-				src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_960_720.jpg"
-			></v-parallax>
+		<div class="mx-auto w-xs-100 w-sm-75 w-xl-50 mt-n16 mb-6 justify-center">
+			<v-container>
+				<v-row>
+					<v-col>
+						<v-card class="mx-lg-16 px-lg-16">
+							<div class="text-center">
+								<v-card-title>
+									ALPHABETA
+									<v-card-subtitle>
+										SOLUÇÕES EM SEGURANÇA ESPECIALIZADA
+									</v-card-subtitle>
+								</v-card-title>
+							</div>
+							<v-divider class="mx-16"></v-divider>
+							<v-card-text>
+								<div class="text-justify text-body-1">
+									A ALPHABETA oferece mão de obra especializada e treinada para atender todas as demandas
+									em serviços terceirizados, tanto para condomínios quanto para empresas. Com mais de 10
+									anos de experiência, entendemos profundamente a importância da transformação digital e
+									das novas dinâmicas nas relações comerciais e sociais no mundo dos negócios.
+									Estamos comprometidos em oferecer soluções inovadoras e eficientes para nossos clientes,
+									adaptando-nos às necessidades do mercado atual. Nossos profissionais são cuidadosamente
+									selecionados e continuamente treinados para garantir a mais alta qualidade de serviço,
+									proporcionando segurança, confiabilidade e excelência operacional.
+									Além disso, utilizamos tecnologias de ponta para otimizar processos e garantir uma gestão
+									eficaz dos recursos, sempre visando a satisfação do cliente e a melhoria contínua. Com a ALPHABETA,
+									você pode contar com um parceiro que entende suas necessidades e está preparado para enfrentá-las
+									com competência e dedicação.
+								</div>
+							</v-card-text>
+						</v-card>
+					</v-col>
+				</v-row>
+			</v-container>
 		</div>
 	</div>
 </template>
