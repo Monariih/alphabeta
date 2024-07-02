78a88bf512c351464e15d269a277fc199a6990c0
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-02 11:45:26 -0300

    implemet text about who is aphabeta

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -9,6 +9,20 @@
 		<div class="mt-n8 position-relative mx-16 d-sm-mx-8">
 			<v-sheet class="mx-16">
 				<div class="text-h5">ALPHABETA - SOLUÇÕES EM SEGURANÇA</div>
+				<div class="text-justify text-body-2 mt-6">
+					A ALPHABETA oferece mão de obra especializada e treinada para atender todas as demandas
+					em serviços terceirizados, tanto para condomínios quanto para empresas. Com mais de 10
+					anos de experiência, entendemos profundamente a importância da transformação digital e
+					das novas dinâmicas nas relações comerciais e sociais no mundo dos negócios.
+					Estamos comprometidos em oferecer soluções inovadoras e eficientes para nossos clientes,
+					adaptando-nos às necessidades do mercado atual. Nossos profissionais são cuidadosamente
+					selecionados e continuamente treinados para garantir a mais alta qualidade de serviço,
+					proporcionando segurança, confiabilidade e excelência operacional.
+					Além disso, utilizamos tecnologias de ponta para otimizar processos e garantir uma gestão
+					eficaz dos recursos, sempre visando a satisfação do cliente e a melhoria contínua. Com a ALPHABETA,
+					você pode contar com um parceiro que entende suas necessidades e está preparado para enfrentá-las
+					com competência e dedicação.
+				</div>
 			</v-sheet>
 		</div>
 	</div>
