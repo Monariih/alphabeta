4351bd77e58b2454d140422f40b4e4e16bded8f1
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-10 14:34:49 -0300

    remove v-card compononent and uses sheets

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -7,30 +7,30 @@
 			></v-parallax>
 		</div>
 	
-		<div class="mx-auto w-xs-100 w-sm-75 w-xl-50 mt-n16 mb-6 justify-center">
-			<v-container>
-				<v-row>
-					<v-col>
-						<v-card class="mx-lg-16 px-lg-16">
-							<div class="text-center">
-								<v-card-title>
-									ALPHABETA
-									<v-card-subtitle>
-										SOLUÇÕES EM SEGURANÇA ESPECIALIZADA
-									</v-card-subtitle>
-								</v-card-title>
-							</div>
-							<v-divider class="mx-16"></v-divider>
-							<v-card-text>
-								<div class="text-justify text-body-1">
-									A ALPHABETA oferece mão de obra especializada e treinada para atender todas as demandas
-									em serviços terceirizados, tanto para condomínios quanto para empresas. Com mais de 10
-									anos de experiência, entendemos profundamente a importância da transformação digital e
-									das novas dinâmicas nas relações comerciais e sociais no mundo dos negócios.
-									Estamos comprometidos em oferecer soluções inovadoras e eficientes para nossos clientes,
-									adaptando-nos às necessidades do mercado atual. Nossos profissionais são cuidadosamente
-									selecionados e continuamente treinados para garantir a mais alta qualidade de serviço,
-									proporcionando segurança, confiabilidade e excelência operacional.
+		<div class="mx-auto w-lg-50 w-md-75 w-sm-75 position-relative mt-n16">
+			<v-sheet class="pa-4 pa-sm-6 pa-md-8 pa-lg-12 rounded">
+				<div class="text-h4 text-center">
+					ALPHABETA
+					<div class="text-caption mt-n2">
+						SERVÇOS ESPECIALIZADOS EM SEGURANÇA E TERCEIRAZAÇÃO
+					</div>
+				</div>
+				<v-divider class="my-2 mx-4"></v-divider>
+				<div class="text-justify">
+					A ALPHABETA oferece mão de obra especializada e treinada para atender todas as demandas
+					em serviços terceirizados, tanto para condomínios quanto para empresas. Com mais de 10
+					anos de experiência, entendemos profundamente a importância da transformação digital e
+					das novas dinâmicas nas relações comerciais e sociais no mundo dos negócios.
+					Estamos comprometidos em oferecer soluções inovadoras e eficientes para nossos clientes,
+					adaptando-nos às necessidades do mercado atual. Nossos profissionais são cuidadosamente
+					selecionados e continuamente treinados para garantir a mais alta qualidade de serviço,
+					proporcionando segurança, confiabilidade e excelência operacional.
+					Além disso, utilizamos tecnologias de ponta para otimizar processos e garantir uma gestão
+					eficaz dos recursos, sempre visando a satisfação do cliente e a melhoria contínua. Com a ALPHABETA,
+					você pode contar com um parceiro que entende suas necessidades e está preparado para enfrentá-las
+					com competência e dedicação.
+				</div>
+			</v-sheet>
 									Além disso, utilizamos tecnologias de ponta para otimizar processos e garantir uma gestão
 									eficaz dos recursos, sempre visando a satisfação do cliente e a melhoria contínua. Com a ALPHABETA,
 									você pode contar com um parceiro que entende suas necessidades e está preparado para enfrentá-las
