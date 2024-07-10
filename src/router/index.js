09ff6c81ea4e6fcb16a01ed9e665eee8a4e4a669
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-10 16:58:37 -0300

    create router main component

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
-import HomePage from '../pages/HomePage.vue'
-import AboutView from './AboutView.vue'
+import HomePage from '@/pages/HomePage.vue'
+import WhoWeAre from '@/pages/WhoWeAre.vue'
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/quem-somos', component: AboutView },
+  { path: '/quem-somos', component: WhoWeAre },
 ]
 
 const router = createRouter({
   history: createMemoryHistory(),
   routes,
 })
+
+export default router
