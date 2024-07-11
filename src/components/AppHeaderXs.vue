918372dafac3d5d617b5b6bab648d057a29a6b18
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-11 12:29:13 -0300

    create the new header for sx screens

---
+++ src/components/AppHeaderXs.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app-bar scroll-behavior="collapse" class="bg-alphabeta-red">
+		<v-app-bar-nav-icon @click.stop="drawer = !drawer">
+		</v-app-bar-nav-icon>
+		<v-toolbar-title>
+			<router-link to="/">
+				<v-img
+					:width="200"
+					src="../assets/alphabeta-logos/logo-txt.png"
+				></v-img>
+			</router-link>
+		</v-toolbar-title>
+	</v-app-bar>
+
+		<v-navigation-drawer
+			v-model="drawer"
+			:location="$vuetify.display.mobile ? 'bottom' : undefined"
+			temporary
+		>
+			<v-list
+				class="bg-alphabeta-red"
+				:items="items"
+			></v-list>
+		</v-navigation-drawer>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		drawer: false,
+		group: null,
+		items: [
+			{ title: 'Home', icon: 'mdi-home', to: '/' },
+			{ title: 'Quem somos', icon: 'mdi-account-group', to: '/quem-somos' },
+			{ title: 'Clientes', icon: 'mdi-account-multiple', to: '/clientes' },
+			{ title: 'Serviços', icon: 'mdi-cog', to: '/servicos' },
+			{ title: 'Fale Conosco', icon: 'mdi-email', to: '/fale-conosco' },
+		],
+	}),
+	watch: {
+		group () {
+			this.drawer = false
+		},
+	}
+}
+</script>
