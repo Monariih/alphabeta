113f653c97158692a5a59395fa234ee534d88929
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-15 14:44:26 -0300

    change initial text position

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -64,6 +64,29 @@
 			</div>
 		</v-row>
 	</div>
+
+			<div class="mx-auto w-lg-50 w-md-75 w-sm-75 position-relative my-12 d-md-rouded-lg">
+			<v-sheet class="pa-4 pa-sm-6 pa-md-8 pa-lg-12">
+				<div class="text-h4 text-center">
+					ALPHABETA
+				</div>
+				<v-divider class="my-2 mx-4"></v-divider>
+				<div class="text-justify">
+					A ALPHABETA oferece mão de obra especializada e treinada para atender todas as demandas
+					em serviços terceirizados, tanto para condomínios quanto para empresas. Com mais de 10
+					anos de experiência, entendemos profundamente a importância da transformação digital e
+					das novas dinâmicas nas relações comerciais e sociais no mundo dos negócios.
+					Estamos comprometidos em oferecer soluções inovadoras e eficientes para nossos clientes,
+					adaptando-nos às necessidades do mercado atual. Nossos profissionais são cuidadosamente
+					selecionados e continuamente treinados para garantir a mais alta qualidade de serviço,
+					proporcionando segurança, confiabilidade e excelência operacional.
+					Além disso, utilizamos tecnologias de ponta para otimizar processos e garantir uma gestão
+					eficaz dos recursos, sempre visando a satisfação do cliente e a melhoria contínua. Com a ALPHABETA,
+					você pode contar com um parceiro que entende suas necessidades e está preparado para enfrentá-las
+					com competência e dedicação.
+				</div>
+			</v-sheet>
+		</div>
 </template>
 
 <script>
