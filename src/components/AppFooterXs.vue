a006376b6e5d669e0d35b6e4e5fdc75ea2ef7e2f
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-18 15:24:19 -0300

    create a footer to small screen sizes

---
+++ src/components/AppFooterXs.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-footer
+    class="bg-grey-darken-4"
+  >
+    <div>
+      <v-row>
+        <v-col>
+          <div>
+            <v-img
+              src="../assets/alphabeta-logos/logo.png"
+              max-height="80"
+            ></v-img>
+            <v-divider class="my-2"></v-divider>
+            <div class="text-center">
+              <v-icon>mdi-map-marker</v-icon>
+              Rua Orense, 41 - Sala 1214 - Diadema - SP
+              <div>
+                <v-icon>mdi-phone</v-icon>
+                (11) 5242-5488
+              </div>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+      socialNetwork: [
+        { title: 'Facebook', icon: 'mdi-facebook', to: 'https://www.facebook.com' },
+        { title: 'Instagram', icon: 'mdi-instagram', to: 'https://www.instagram.com' },
+        { title: 'LinkedIn', icon: 'mdi-linkedin', to: 'https://www.linkedin.com' }
+      ],
+      contactForms: [
+        { title: 'Email', icon: 'mdi-email', to: 'mailto:' },
+        { title: 'Telefone', icon: 'mdi-phone', to: 'tel:' },
+        { title: 'WhatsApp', icon: 'mdi-whatsapp', to: 'https://api.whatsapp.com/send?phone='}
+      ]
+    }
+  }
+}
+</script>
