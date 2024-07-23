6b2d37b31153fe40f8233ff4831ca20fc713945e
Lucas Monari <lucas.monari.lm@outlook.com>
2024-07-23 10:46:02 -0300

    set text and image as responsive

--- src/pages/WhoWeAre.vue
+++ src/pages/WhoWeAre.vue
@@ -1,28 +1,51 @@
 <template>
 	<div>
-		<v-sheet class="my-6">
-			<v-container>
-				<v-row no-gutters class="mx-auto w-lg-50 w-md-75 w-sm-75 my-12">
-					<v-col class="">
-						<div>
-							<v-img 
-								:width="200"
-								src="https://scontent.fgru6-1.fna.fbcdn.net/v/t1.6435-9/93136940_105502114464058_2667872051770949632_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=0b6b33&_nc_ohc=3iCZvxSq6tkQ7kNvgEQmPUd&_nc_ht=scontent.fgru6-1.fna&oh=00_AYCEBeuUtpXr80uCbfG8vE4w34nJ7kgxl5SK4m2FYnHBfw&oe=66C5D617"
-							></v-img>
-						</div>
-					</v-col>
-					<v-col>
-						<v-sheet class="pa-4">
-							<div class="text-h4">
-								Quem somos
-							</div>
-							<div class="text-justify">
-								A Alphabeta é uma empresa especializada em segurança e terceirização para condomínios e empresas. Atuamos no mercado desde 2000 e temos como missão oferecer soluções personalizadas e inovadoras para nossos clientes.
-							</div>
-						</v-sheet>
-					</v-col>
-				</v-row>
-			</v-container>
+		<v-sheet class="mt-12">
+			<v-row no-gutters align="center" class="mx-auto w-lg-50 w-md-75 w-sm-75">
+				<v-col justify="end">
+					<div
+						v-if="cols == 6"
+						class="ml-16"
+					>
+						<v-img 
+							src="https://scontent.fgru6-1.fna.fbcdn.net/v/t1.6435-9/93136940_105502114464058_2667872051770949632_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=0b6b33&_nc_ohc=3iCZvxSq6tkQ7kNvgEQmPUd&_nc_ht=scontent.fgru6-1.fna&oh=00_AYCEBeuUtpXr80uCbfG8vE4w34nJ7kgxl5SK4m2FYnHBfw&oe=66C5D617"
+							height="300"
+						></v-img>
+					</div>
+					<div
+						v-if="cols == 12"
+					>
+						<v-img 
+							src="https://scontent.fgru6-1.fna.fbcdn.net/v/t1.6435-9/93136940_105502114464058_2667872051770949632_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=0b6b33&_nc_ohc=3iCZvxSq6tkQ7kNvgEQmPUd&_nc_ht=scontent.fgru6-1.fna&oh=00_AYCEBeuUtpXr80uCbfG8vE4w34nJ7kgxl5SK4m2FYnHBfw&oe=66C5D617"
+							height="400"
+						></v-img>
+					</div>
+				</v-col>
+				<v-col
+					v-if="cols == 6"
+				>
+					<div>
+						<p class="text-h4">
+							{{ whoWeAre.title }}
+						</p>
+						<p class="text-body-1 text-justify">
+							{{ whoWeAre.text }}
+						</p>
+					</div>
+				</v-col>
+				<div
+					v-if="cols == 12"
+					class="text-justify pa-4"
+				>
+					<p class="text-h4">
+						{{ whoWeAre.title }}
+					</p>
+					<p class="text-body-1 text-justify">
+						{{ whoWeAre.text }}
+					</p>
+				</div>
+			</v-row>
+		</v-sheet>
 		</v-sheet>
 	</div>
 </template>
