ffcdb789b499ef893c24d374ab783dcb50bf5d8d
Lucas Monari <lucas.monari.lm@outlook.com>
2024-09-09 19:32:17 -0300

    remove hardcoded text and add it to js

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -72,18 +72,7 @@
 			</div>
 			<v-divider class="my-2 mx-4"></v-divider>
 			<div class="text-justify">
-				A ALPHABETA oferece mão de obra especializada e treinada para atender todas as demandas
-				em serviços terceirizados, tanto para condomínios quanto para empresas. Com mais de 10
-				anos de experiência, entendemos profundamente a importância da transformação digital e
-				das novas dinâmicas nas relações comerciais e sociais no mundo dos negócios.
-				Estamos comprometidos em oferecer soluções inovadoras e eficientes para nossos clientes,
-				adaptando-nos às necessidades do mercado atual. Nossos profissionais são cuidadosamente
-				selecionados e continuamente treinados para garantir a mais alta qualidade de serviço,
-				proporcionando segurança, confiabilidade e excelência operacional.
-				Além disso, utilizamos tecnologias de ponta para otimizar processos e garantir uma gestão
-				eficaz dos recursos, sempre visando a satisfação do cliente e a melhoria contínua. Com a ALPHABETA,
-				você pode contar com um parceiro que entende suas necessidades e está preparado para enfrentá-las
-				com competência e dedicação.
+				{{ primaryText }}
 			</div>
 		</v-sheet>
 	</div>
@@ -115,6 +104,19 @@ export default {
 					text: "Segurança para eventos corporativos, sociais, esportivos e culturais. Equipe especializada em segurança e atendimento ao público. Controle de acesso, segurança patrimonial, brigada de incêndio e muito mais. Entre em contato conosco e solicite um orçamento."
 				},
 			],
+			primaryText:
+				`A ALPHABETA oferece mão de obra especializada e treinada para atender todas as demandas
+				em serviços terceirizados, tanto para condomínios quanto para empresas.Com mais de 10
+				anos de experiência, entendemos profundamente a importância da transformação digital e
+				das novas dinâmicas nas relações comerciais e sociais no mundo dos negócios.
+				Estamos comprometidos em oferecer soluções inovadoras e eficientes para nossos clientes,
+				adaptando-nos às necessidades do mercado atual. Nossos profissionais são cuidadosamente
+				selecionados e continuamente treinados para garantir a mais alta qualidade de serviço,
+				proporcionando segurança, confiabilidade e excelência operacional.
+				Além disso, utilizamos tecnologias de ponta para otimizar processos e garantir uma gestão
+				eficaz dos recursos, sempre visando a satisfação do cliente e a melhoria contínua. Com a ALPHABETA,
+				você pode contar com um parceiro que entende suas necessidades e está preparado para enfrentá-las
+				com competência e dedicação .`,
 		}
 	},
 	computed: {
