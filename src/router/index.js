1ab7cd84dc6a2742ee07c6357cd71248484d48c5
Lucas Monari <lucas.monari.lm@outlook.com>
2024-09-09 20:17:42 -0300

    add new page to the router

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 
 import HomePage from '@/pages/HomePage.vue'
 import WhoWeAre from '@/pages/WhoWeAre.vue'
+import ContactUs from '@/pages/ContactUs.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/quem-somos', component: WhoWeAre },
+  { path: '/fale-conosco', component: ContactUs },
 ]
 
 const router = createRouter({
