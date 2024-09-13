264185c485ce1236b388d70a2a44303bfc1366c4
Lucas Monari <lucas.monari.lm@outlook.com>
2024-09-12 22:54:28 -0300

    add all the cards to contact methods

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,5 +1,261 @@
 <template>
-  <div>
-    FALA CUNOIS
-  </div>
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 6">
+		<v-col class="d-flex" cols="6" offset="3">
+      <v-card append-icon="mdi-whatsapp" title="WhatsApp" class="w-100">
+        <v-card-text>
+          Estamos prontos para te atender via whatsapp através do número (11) 5242-5488. ou clique no botão abaixo.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="success"
+            href="https://api.whatsapp.com/send?phone=551152425488"
+            target="_blank"
+            variant="outlined"
+            block
+          >
+            Fale conosco
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 6">
+    <v-col class="d-flex" cols="6" offset="3">
+      <v-card append-icon="mdi-facebook" title="Facebook" class="w-100">
+        <v-card-text>
+          Nós estamos disponíveis no Facebook, siga a gente para acompanhar as novidades em tempo real! Clique no botão abaixo para acessar nossa página.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="primary"
+            href="https://api.whatsapp.com/send?phone=551152425488"
+            target="_blank"
+            variant="outlined"
+            block
+          >
+            Facebook Alphabeta
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 6">
+    <v-col class="d-flex" cols="6" offset="3">
+      <v-card append-icon="mdi-instagram" title="Instagram" class="w-100">
+        <v-card-text>
+          Ainda estamos trabalhando em nossa página para apresentar a vocês o melhor conteúdo possível. Aguarde!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="error"
+            href="https://api.whatsapp.com/send?phone=551152425488"
+            target="_blank"
+            variant="outlined"
+            block
+            disabled
+          >
+            Instagram Alphabeta
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 6">
+    <v-col class="d-flex" cols="6" offset="3">
+      <v-card append-icon="mdi-email" title="E-mail" class="w-100">
+        <v-card-text>
+          Contacte a gente através do e-mail alphabeta@alphabeta.com.br. Estamos prontos para te atender!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="yellow"
+            href="mailto:alphabeta@alphabeta.com.br"
+            target="_blank"
+            variant="outlined"
+            block
+          >
+            E-mail
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 6">
+    <v-col class="d-flex" cols="6" offset="3">
+      <v-card append-icon="mdi-linkedin" title="LinkedIn" class="w-100">
+        <v-card-text>
+          Vamos com calma, ainda estamos trabalhando em nossa página no LinkedIn. Aguarde!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="blue"
+            href="https://www.linkedin.com"
+            target="_blank"
+            variant="outlined"
+            block
+            disabled
+          >
+            LinkedIn
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+    <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 6">
+    <v-col class="d-flex" cols="6" offset="3">
+      <v-card append-icon="mdi-phone" title="Telefone" class="w-100">
+        <v-card-text>
+          Estamos prontos para te atender via telefone através do número (11) 5242-5488. Liga pra gente, vai ser um prazer atende-lo!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="blue"
+            target="_blank"
+            variant="outlined"
+            block
+          >
+            (11) 5242-5488
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+
+
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 12">
+		<v-col class="d-flex">
+      <v-card append-icon="mdi-whatsapp" title="WhatsApp" class="w-100">
+        <v-card-text>
+          Estamos prontos para te atender via whatsapp através do número (11) 5242-5488. ou clique no botão abaixo.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="success"
+            href="https://api.whatsapp.com/send?phone=551152425488"
+            target="_blank"
+            variant="outlined"
+            block
+          >
+            Fale conosco
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 12">
+    <v-col class="d-flex">
+      <v-card append-icon="mdi-facebook" title="Facebook" class="w-100">
+        <v-card-text>
+          Nós estamos disponíveis no Facebook, siga a gente para acompanhar as novidades em tempo real! Clique no botão abaixo para acessar nossa página.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="primary"
+            href="https://api.whatsapp.com/send?phone=551152425488"
+            target="_blank"
+            variant="outlined"
+            block
+          >
+            Facebook Alphabeta
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 12">
+    <v-col class="d-flex">
+      <v-card append-icon="mdi-instagram" title="Instagram" class="w-100">
+        <v-card-text>
+          Ainda estamos trabalhando em nossa página para apresentar a vocês o melhor conteúdo possível. Aguarde!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="error"
+            href="https://api.whatsapp.com/send?phone=551152425488"
+            target="_blank"
+            variant="outlined"
+            block
+            disabled
+          >
+            Instagram Alphabeta
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 12">
+    <v-col class="d-flex">
+      <v-card append-icon="mdi-email" title="E-mail" class="w-100">
+        <v-card-text>
+          Contacte a gente através do e-mail alphabeta@alphabeta.com.br. Estamos prontos para te atender!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="yellow"
+            href="mailto:alphabeta@alphabeta.com.br"
+            target="_blank"
+            variant="outlined"
+            block
+          >
+            E-mail
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 12">
+    <v-col class="d-flex">
+      <v-card append-icon="mdi-linkedin" title="LinkedIn" class="w-100">
+        <v-card-text>
+          Vamos com calma, ainda estamos trabalhando em nossa página no LinkedIn. Aguarde!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="blue"
+            href="https://www.linkedin.com"
+            target="_blank"
+            variant="outlined"
+            block
+            disabled
+          >
+            LinkedIn
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+    <v-row no-gutters align="center" class="my-8 w-xl-50 mx-auto w-lg-75" v-if="cols == 12">
+    <v-col class="d-flex">
+      <v-card append-icon="mdi-phone" title="Telefone" class="w-100">
+        <v-card-text>
+          Estamos prontos para te atender via telefone através do número (11) 5242-5488. Liga pra gente, vai ser um prazer atende-lo!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="blue"
+            target="_blank"
+            variant="outlined"
+            block
+          >
+            (11) 5242-5488
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
+<script>
+export default {
+  setup() {
+    return {
+
+    }
+  },
+  computed: {
+    cols () {
+      const { lg, xs } = this.$vuetify.display
+      return lg ? [6] : xs ? [12] : [6]
+    },
+	}
+};
+</script>
