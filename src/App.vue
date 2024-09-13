9b4132751ca7ca123ff281f46f719333f2033ad5
Lucas Monari <lucas.monari.lm@outlook.com>
2024-09-12 22:52:40 -0300

    add a whatsapp floating btn to all the app

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
     <AppHeaderXs v-if="!screenSize" />
     <v-main class="bg-alphabeta-background">
       <router-view />
+      <!-- Botão flutuante do WhatsApp -->
+      <a
+        href="https://api.whatsapp.com/send?phone=551152425488"
+        target="_blank"
+        class="whatsapp-float"
+      >
+        <v-icon class="whatsapp-icon">mdi-whatsapp</v-icon>
+      </a>
     <AppFooter v-if="screenSize" />
     <AppFooterXs v-if="!screenSize"/>
     </v-main>
@@ -22,3 +30,26 @@ export default {
   }
 }
 </script>
+
+<style>
+.whatsapp-float {
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  bottom: 40px;
+  right: 40px;
+  background-color: #25d366;
+  color: #fff;
+  border-radius: 50px;
+  text-align: center;
+  font-size: 30px;
+  box-shadow: 2px 2px 3px #999;
+  z-index: 1000;
+}
+
+.whatsapp-icon {
+  font-size: 45px;
+	margin: 7px 0 0 3px;
+  color: #fff;
+}
+</style>
